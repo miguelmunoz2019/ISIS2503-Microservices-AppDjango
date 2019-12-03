4d44d3eb72024369d168111483c4abcc9000a74c
miguelmunoz2019 <michaelalmos10@gmail.com>
2019-12-03 16:55:31 -0500

    avances

--- productos/productos/models.py
+++ productos/productos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Producto(models.Model):
-    id = models.FloatField(null=True, blank=True, default=None)
+    id = models.FloatField(primary_key=True, default=None)
     nombre = models.CharField(max_length=50)
     marca = models.CharField(max_length=50)
     precio = models.FloatField(null=True, blank=True, default=None)
