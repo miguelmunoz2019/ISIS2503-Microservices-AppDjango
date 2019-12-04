fa70a4798767992d00bc6f98ca596cd9aac47dba
miguelmunoz2019 <michaelalmos10@gmail.com>
2019-12-04 01:58:57 -0500

    avances

--- clientes/clientes/models.py
+++ clientes/clientes/models.py
@@ -1,16 +1,13 @@
 from django.db import models
 
 
-class ProductoCarrito(models.Model):
-    id = models.IntegerField(primary_key=True)
-    nombre = models.CharField(max_length=50)
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length=50)
     id = models.IntegerField(primary_key=True)
     direccion = models.CharField(max_length=50)
     correo = models.CharField(max_length=50)
-    carrito = models.ManyToManyField(ProductoCarrito)
+    carrito = models.CharField(max_length=50)
 
 
     def __str__(self):
--- clientes/clientes/views.py
+++ clientes/clientes/views.py
@@ -1,4 +1,4 @@
-from .models import Cliente,ProductoCarrito
+from .models import Cliente, ProductoCarrito
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.http import HttpResponse
@@ -11,7 +11,7 @@ import json
 
 
 def check_producto(data):
-    r = requests.get(settings.PATH_VAR, headers={"Accept":"application/json"})
+    r = requests.get(settings.PATH_VAR, headers={"Accept": "application/json"})
     productos = r.json()
     for producto in productos:
         if data["nombre"] == producto["id"]:
@@ -21,7 +21,7 @@ def check_producto(data):
 
 def cliente_list(request):
     queryset = Cliente.objects.all()
-    context = list(queryset.values('nombre', 'id', 'direccion', 'correo' ))
+    context = list(queryset.values('nombre', 'id', 'direccion', 'correo'))
     return JsonResponse(context, safe=False)
 
 
@@ -29,36 +29,21 @@ def ClienteCreate(request):
     if request.method == 'POST':
         data = request.body.decode('utf-8')
         data_json = json.loads(data)
-
         cliente = Cliente()
-        producto = ProductoCarrito()
-        producto.id = 0
-        producto.nombre = "nulo"
-        cliente.id = data_json['id']
-        cliente.nombre = data_json['nombre']
-        cliente.direccion = data_json['direccion']
-        cliente.correo = data_json['correo']
-        cliente.save()
-        return HttpResponse("successfully created cliente")
-
-
-def AddProductoCarro(request):
-    if request.method == 'PUT':
-        data = request.body.decode('utf-8')
-        data_json = json.loads(data)
-
-        if check_producto(data_json):
-            cliente = data_json['id']
-            producto = ProductoCarrito()
-            producto.id = data_json['nombre']
-            producto.nombre = data_json['nombre2']
-            updateSQL(cliente, producto)
-        return HttpResponse("successfully created cliente")
-
-
-def updateSQL(id,producto):
-    for p in Cliente.objects.raw('SELECT * FROM '+ 'clientes_Cliente '+ 'WHERE id ='+ str(id)):
-        p.carrito.add(producto)
-        p.save()
-
-
+        if cliente.carrito == "":
+            cliente.id = data_json['id']
+            cliente.nombre = data_json['nombre']
+            cliente.direccion = data_json['direccion']
+            cliente.correo = data_json['correo']
+            cliente.carrito = data_json['carrito']
+            cliente.save()
+            return HttpResponse("successfully created cliente")
+        elif check_producto(data_json):
+            cliente.id = data_json['id']
+            cliente.nombre = data_json['nombre']
+            cliente.direccion = data_json['direccion']
+            cliente.correo = data_json['correo']
+            cliente.carrito = data_json['carrito']
+            cliente.save()
+            return HttpResponse("successfully added producto to carrito")
+        return HttpResponse("ERROR")
