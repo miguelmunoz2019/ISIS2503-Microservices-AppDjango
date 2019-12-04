0893ff5294aafae5a4f40d169456afffcb1051e2
miguelmunoz2019 <michaelalmos10@gmail.com>
2019-12-04 00:25:43 -0500

    avances

--- clientes/clientes/models.py
+++ clientes/clientes/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 
+class ProductoCarrito(models.Model):
+    id = models.IntegerField(primary_key=True)
+
+
 class Cliente(models.Model):
     nombre = models.CharField(max_length=50)
     id = models.IntegerField(primary_key=True)
     direccion = models.CharField(max_length=50)
     correo = models.CharField(max_length=50)
-    carrito = models.IntegerField(null=False, default=None)
+    carrito = models.ManyToManyField(ProductoCarrito)
+
 
     def __str__(self):
         return '%s %s' % (self.value, self.unit)
--- clientes/clientes/views.py
+++ clientes/clientes/views.py
@@ -8,6 +8,7 @@ from django.conf import settings
 import requests
 import json
 
+
 def check_producto(data):
     r = requests.get(settings.PATH_VAR, headers={"Accept":"application/json"})
     productos = r.json()
@@ -16,23 +17,23 @@ def check_producto(data):
             return True
     return False
 
+
 def cliente_list(request):
     queryset = Cliente.objects.all()
-    context = list(queryset.values('nombre', 'id', 'direccion', 'correo', 'carrito'))
+    context = list(queryset.values('nombre', 'id', 'direccion', 'correo' ))
     return JsonResponse(context, safe=False)
+
 
 def ClienteCreate(request):
     if request.method == 'POST':
         data = request.body.decode('utf-8')
         data_json = json.loads(data)
-        if check_producto(data_json) == True:
-            cliente = Cliente()
-            cliente.nombre = data_json['nombre']
-            cliente.id = data_json['id']
-            cliente.direccion = data_json['direccion']
-            cliente.correo = data_json['correo']
-            cliente.carrito = data_json['carrito']
-            producto.save()
-            return HttpResponse("successfully created cliente")
-        else:
-            return HttpResponse("unsuccessfully created cliente. producto does not exist")
+
+        cliente = Cliente()
+        cliente.id = data_json['id']
+        cliente.nombre = data_json['nombre']
+        cliente.direccion = data_json['direccion']
+        cliente.correo = data_json['correo']
+
+        producto.save()
+        return HttpResponse("successfully created cliente")
