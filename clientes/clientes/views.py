77a19f07586abf42e9e6f0dd87656727befa489f
miguelmunoz2019 <michaelalmos10@gmail.com>
2019-12-04 02:10:15 -0500

    avances

--- clientes/clientes/views.py
+++ clientes/clientes/views.py
@@ -32,15 +32,7 @@ def ClienteCreate(request):
         data = request.body.decode('utf-8')
         data_json = json.loads(data)
         cliente = Cliente()
-        if cliente.carrito == "":
-            cliente.id = data_json['id']
-            cliente.nombre = data_json['nombre']
-            cliente.direccion = data_json['direccion']
-            cliente.correo = data_json['correo']
-            cliente.carrito = data_json['carrito']
-            cliente.save()
-            return HttpResponse("successfully created cliente")
-        elif check_producto(data_json):
+        if check_producto(data_json):
             cliente.id = data_json['id']
             cliente.nombre = data_json['nombre']
             cliente.direccion = data_json['direccion']
