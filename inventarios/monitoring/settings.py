4efdb2ea27fbaa52c156cc858389c26885daad4f
Ubuntu <ubuntu@ip-172-31-88-95.ec2.internal>
2019-12-04 12:38:17 +0000

    avances

--- inventarios/monitoring/settings.py
+++ inventarios/monitoring/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'inventarios_db',
-        'USER': 'inventarios_user',
+        'NAME': 'variables_db',
+        'USER': 'variables_user',
         'PASSWORD': 'isis2503',
-        'HOST': '0.0.0.0',
+        'HOST': '3.88.215.220',
         'PORT': '5432',
     }
 }
